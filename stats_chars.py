dad787af01b23f391e1e15c3db99aa8120f2dfc6
Sam Sepiol <samsepiol@somewhere.com>
2023-01-27 03:07:19 -0600

    now makes insertions

--- stats_chars.py
+++ stats_chars.py
@@ -1,4 +1,3 @@
-import subprocess
 import mysql.connector
 
 def connection(host, user, password, database, port):
@@ -14,21 +13,36 @@ def connection(host, user, password, database, port):
       print('[+] Connected to the database!')
    return mydb,cursor
 
-"""
 def insert_stats(stats):
-   char = {}
+   values = []
    for stat in stats:
       res = input('Insert {}: '.format(stat))
-      char[stat] = res
-   return char 
-"""
+      values.append(res)
+   return values 
 
-stats = ["name", "magic_level", "sword", "axe", "distance", "shielding"]
-#char = insert_stats(stats)
-#for key,value in char.items():
-#   print('{} : {}'.format(key,value))
+def insert_char(stats, values, mydb, cursor):
+   stat = ''
+   val = ''
+   val = ', '.join(['%s'] * len(values))
+   stat = ', '.join(stat)
+   insert_query = ''' INSERT INTO characters (%s) values (%s)'''%(stat, val)
+   cursor.execute(insert_query, values)
+   mydb.commit()
+   print('[+] Char stats added correctly!')
 
+def get_chars(mydb, cursor):
+   cursor.execute('select * from characters')
+   result = cursor.fetchall()
+   return result
+
+stats = ["first_name", "shielding", "magic_level", "sword", "axe", "distance"]
+values = insert_stats(stats)
+print(values)
 mydb, cursor = connection('localhost', 'tibia', 'tibia123', 'tibia', '3309')
-cursor.execute('show create table characters')
-resultado = cursor.fetchall()
-print(resultado)
+insert_char(stats, values, mydb, cursor)
+result = get_chars(mydb, cursor)
+print(result)
+
+#cursor.execute('show create table characters')
+#resultado = cursor.fetchall()
+#print(resultado)
